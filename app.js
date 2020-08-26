806d14138134bf9c5a9ff8102cabd39028bd9cba
iamarvind <varoonarvind287054@gmail.com>
2020-08-26 12:57:18 +0530

    to make a retrieve task feature ,also have a clear all option to clear all deleted tasks.

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+// functions
+const checkToDelete = (list) => {
+  return Array.from(list).includes("delete");
+};
+
+const checkToAdd = (list) => {
+  return Array.from(list).includes("add-task");
+};
+
+// this is the todo users todo list.
 const todoList = document.querySelector(".todo-list");
+// this is the button to add new list.
 const addButton = document.querySelector(".add-button");
+// this is the form that is used to get new task name
 const form = document.querySelector(".task-form");
+// ul where the deleted tasks to be added when show deleted is clicked.
+const hiddenList = document.querySelector(".deleted-list");
+//this is the button to show deleted list.
+const showButton = document.querySelector(".show-button");
+//this is the button to clear the all deleted task.
+const clearAll = document.querySelector(".clear-list");
 
 /* To toggle the form display */
 addButton.addEventListener("click", () => {
   form.classList.toggle("hidden");
+  form["new-task"].value = "";
 });
 
 /* To add a new task */
@@ -19,20 +38,65 @@ form.addEventListener("submit", (event) => {
   task.setAttribute("class", "todo-para");
 
   const icon = document.createElement("i");
-  icon.setAttribute("class", "far fa-trash-alt todo-icon");
+  icon.setAttribute("class", "far fa-trash-alt todo-icon delete");
 
   taskBox.append(task);
   taskBox.append(icon);
   todoList.append(taskBox);
 
-  form["new-task"].value = "";
   form.classList.toggle("hidden");
 });
 
 /* To delete a task */
 todoList.addEventListener("click", (event) => {
   const element = event.target;
-  if (element.tagName === "I") {
-    element.parentNode.remove();
+  if (checkToDelete(element.classList)) {
+    const deleteIcon = document.createElement("i");
+    deleteIcon.setAttribute("class", element.getAttribute("class"));
+    element.setAttribute("class", "fas fa-redo todo-icon add-task");
+    element.parentNode.append(deleteIcon);
+    hiddenList.append(element.parentNode);
+
+    if (todoList.innerHTML === "" && hiddenList.innerHTML !== "") {
+      hiddenList.parentNode.classList.toggle("hidden");
+    }
+  }
+});
+
+/*To show the deleted tasks*/
+showButton.addEventListener("click", () => {
+  if (hiddenList.innerHTML !== "") {
+    hiddenList.parentNode.classList.toggle("hidden");
+  }
+});
+
+/*To permanently delete the task */
+hiddenList.addEventListener("click", (event) => {
+  const element = event.target;
+  if (checkToDelete(element.classList)) {
+    const deleteOrNo = confirm("Do you want to delete the task from history?");
+    if (deleteOrNo) {
+      const parentElement = element.parentNode;
+      parentElement.remove();
+    }
+  } else if (checkToAdd(element.classList)) {
+    const parentElement = element.parentNode;
+    parentElement.querySelector(".add-task").remove();
+    todoList.append(parentElement);
+  }
+  /* to delete the clear list button if the hidden list is empty */
+  if (hiddenList.innerHTML === "") {
+    hiddenList.parentNode.classList.toggle("hidden");
+  }
+});
+
+/*To clear all the deleted tasks from history*/
+clearAll.addEventListener("click", () => {
+  const deleteOrNo = confirm(
+    "Do you want to clear history of all deleted tasks?"
+  );
+  if (deleteOrNo) {
+    hiddenList.innerHTML = "";
+    hiddenList.parentNode.classList.toggle("hidden");
   }
 });
